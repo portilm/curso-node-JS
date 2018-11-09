86fa1235a9f133e35fd0ed311f857904ea65c9fd
Mario <portilm@gmail.com>
2018-11-08 19:15:51 -0600

    /clase2:
    manejo varios de variables

---
+++ clase2.js
@@ -0,0 +1,34 @@
+var numero1 =1;
+var numero2 =2;
+varresultado = numero1 +numero2;
+let num1 =1;
+let num2 =2;
+let res = num1 + num2;
+consoloe.log(resultado, res);
+
+
+if(true){
+    const resultado = nuemero1 + numero2;
+    let res = nume + num2;
+    console.log(`Resultado var: $(resultado), Resultado let: $(res)`);
+    }
+    let arreglo = [1,2,3,4,5];
+    let objeto = {
+    att: `hola mundo`,
+    att2: `hola mundo 2`
+    }
+    console.log(arreglo, objeto);
+
+
+process.argv.array.forEach((value, index))=>{}
+console.log(process.argv);
+
+
+for(let i=0, i< process.argv,length; i++){
+    console.log(`ìndice: ${1}, value: ${process.argv[1]})
+}
+
+let funcion = (param1, param2)=Z{
+    return param1 + param2;
+}
+console.log
